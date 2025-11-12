020e94c254d3b4088a18598ca26003a9c981e6d0
AgnesM12 <agnesmonika0903@gmail.com>
2025-11-12 23:50:50 +0700

    perbaikan

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,12 +14,14 @@ export default {
         xl: '5rem',
       },
     },
-    screens: {
-      'mobile': {'max': '481px'},  
-      'tablet': {'max': '768px'}, 
+    screens: { 
+    'mobile': {'max': '480px'},
+    'tablet': {'min': '481px', 'max': '1023px'},
+    'desktop': {'min': '1024px'},
       'sm': '640px',
       'md': '768px',
-      'lg': '1024px',              
+      'lg': '1024px',
+      'xl': '1280px',              
       '2xl': '1536px',
     },
     extend: {
