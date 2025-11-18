a8f2ff0d81744c667ae198ba6fdb7dab15992ad9
AgnesM12 <agnesmonika0903@gmail.com>
2025-11-18 18:57:39 +0700

    perbaikan

--- src/components/HalamanPaketPenuh.jsx
+++ src/components/HalamanPaketPenuh.jsx
@@ -67,7 +67,7 @@ function HalamanPaketPenuh() {
             description: "Nikmati berbagai pilihan paket wisata di Lovina mulai dari tur lumba-lumba, snorkeling, hingga private tour. Setiap paket dirancang agar perjalanan Anda lebih praktis, seru, dan berkesan"
             }} />
         {/* Grid semua paket */}
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 mt-10">
+        <div className="grid grid-cols-2 md:grid-cols-2 xl:grid-cols-3 gap-4 sm:gap-6 md:gap-8 mt-10 justify-items-center">
             {paketData.map((paket) => (
             <PaketCard key={paket.id} paket={paket} />
             ))}
--- src/components/PaketCard.jsx
+++ src/components/PaketCard.jsx
@@ -7,17 +7,17 @@ const PaketCard = ({ paket }) => {
     const slug = paket.title.toLowerCase().replace(/\s+/g, "-");
 
     return (
-    <div className="w-[320px] sm:w-[320px] md:w-[360px] lg:w-[385px] h-auto p-6 sm:p-5 lg:p-6 bg-white rounded-2xl shadow-[0px_6px_40px_0px_rgba(0,94,209,0.16)] overflow-hidden flex flex-col items-center justify-center transition-transform duration-300">
+    <div className="w-[220px] sm:w-[290px] md:w-[360px] lg:w-[385px] h-auto p-3 sm:p-5 lg:p-6 bg-white rounded-2xl shadow-[0px_6px_40px_0px_rgba(0,94,209,0.16)] overflow-hidden flex flex-col items-center justify-center transition-transform duration-300">
         <img className="w-full aspect-[1.2/1] object-cover object-center rounded-xl" src={paket.imageSrc} alt={paket.title} />
-        <div className="p-4 sm:p-5 flex flex-col gap-3 text-center sm:text-left">
-            <h3 className="text-xl sm:text-xl md:text-2xl font-bold object-center text-gray-900">{paket.title}</h3>
+        <div className="p-2.5 sm:p-5 flex flex-col gap-3  sm:text-left">
+            <h3 className="text-base sm:text-xl md:text-2xl font-bold object-center text-gray-900">{paket.title}</h3>
             <p className="text-base sm:text-base md:text-lg text-gray-600 line-clamp-3">{paket.description}</p>
             <div className="flex flex-row justify-between sm:flex-row sm:justify-between sm:items-center mt-3 gap-2">
-                <p className="text-lg sm:text-xl font-bold text-black">{paket.price}</p>
+                <p className="text-base sm:text-xl font-bold text-black">{paket.price}</p>
                 <div className="flex items-center justify-center sm:justify-end gap-1 text-gray-500">
-                    <HiStar className="h-5 w-5 text-yellow-400" />
-                    <span className="text-zinc-500 text-base font-medium">{paket.rating}</span>
-                    <span className='text-zinc-500 text-base font-medium'>({paket.reviews})</span>
+                    <HiStar className="h-5 w-5 sm:w-6 sm:h-6 text-yellow-400" />
+                    <span className="text-zinc-500 text-sm sm:text-lg md:text-xl font-medium">{paket.rating}</span>
+                    <span className='text-zinc-500 text-sm sm:text-lg md:text-xl font-medium'>({paket.reviews})</span>
                 </div>
             </div>
             <Link to={`/paket/${slug}`} >
