c319dd843fd72595b6ab8fc4467fe8215ac37024
ifanipatanduk <ifanipatanduk@gmail.com>
2025-12-04 23:52:43 +0700

    Update menu paket lengkap

--- src/components/PaketCard.jsx
+++ src/components/PaketCard.jsx
@@ -7,10 +7,10 @@ const PaketCard = ({ paket }) => {
     const slug = paket.title.toLowerCase().replace(/\s+/g, "-");
 
     return (
-    <div className="w-full h-[300px] sm:w-[320px] md:w-[360px] lg:w-[385px] h-auto p-6 sm:p-5 lg:p-6 bg-white rounded-2xl shadow-[0px_6px_40px_0px_rgba(0,94,209,0.16)] overflow-hidden flex flex-col justify-center transition-transform duration-300">
+    <div className="w-full h-[500px] sm:w-[320px] md:w-[360px] lg:w-[385px] h-auto p-6 sm:p-5 lg:p-6 bg-white rounded-2xl shadow-[0px_6px_40px_0px_rgba(0,94,209,0.16)] overflow-hidden flex flex-col justify-center transition-transform duration-300">
         <img className="w-full h-[260px] object-cover object-center rounded-xl" src={paket.imageSrc} alt={paket.title} />
         <div className="mt-2 p-2 sm:p-2 flex flex-col gap-1 text-center sm:text-left">
-            <h3 className="text-xl sm:text-xl md:text-2xl font-bold text-gray-900 text-start">{paket.title}</h3>
+            <h3 className="text-xl sm:text-xl md:text-lg font-bold text-gray-900 text-start">{paket.title}</h3>
             <p className="text-base sm:text-base md:text-lg text-gray-600">{paket.description}</p>
             <div className="flex flex-row justify-between sm:flex-row sm:justify-between sm:items-center gap-2">
                 <p className="text-lg sm:text-xl font-bold text-black">{paket.price}</p>
